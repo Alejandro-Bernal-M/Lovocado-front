1e1fc793609fd5127f75224d9ab371b0d6fdb914
Alejandro Bernal Marin <alejandrober96@gmail.com>
2024-04-22 20:20:11 -0500

    Add signup form

--- src/components/session/Signup.tsx
+++ src/components/session/Signup.tsx
@@ -1,7 +1,45 @@
 export default function Signup() {
+  function handleSignup(e: any) {
+    e.preventDefault();
+    const data = new FormData(e.target);
+    try {
+      fetch(process.env.NEXT_PUBLIC_API + "/signup", {
+        method: "POST",
+        body: data,
+      })
+        .then((res) => {
+          if(res.ok){
+            e.target.reset()
+            return res.json()
+          }else{
+            throw new Error('error')
+          }
+        }
+        )
+        .then((data) => {
+          console.log('success')
+          console.log(data);
+        });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   return (
     <div>
       <h1> Signup Page tittle</h1>
+      <form encType="multipart/form-data"  onSubmit={handleSignup}>
+        <input name="firstName" type="text" placeholder="firstName" />
+        <input name="lastName" type="text" placeholder="lastName" />
+        <input name="email" type="email" placeholder="email" />
+        <input name="username" type="text" placeholder="username" />
+        <input name="contactNumber" type="text" placeholder="contact number" />
+        <input name="password" type="password" placeholder="password" />
+        <input name="passwordConfirmation" type="password" placeholder="password confirmation" />
+        <input name="profileImage" type="file" />
+
+        <button >Signup</button>
+      </form>
     </div>
   );
 }
