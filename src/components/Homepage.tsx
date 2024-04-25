fd8882aedd40e3c0e8c360aed92bc8bd8ffb0ea9
Alejandro Bernal Marin <alejandrober96@gmail.com>
2024-04-24 20:07:50 -0500

    update state display

--- src/components/Homepage.tsx
+++ src/components/Homepage.tsx
@@ -1,11 +1,11 @@
 'use client'
 import { useAppSelector} from "../lib/hooks";
 export default function Homepage() {
-  const {name} = useAppSelector((state) => state.user);
+  const {user} = useAppSelector((state) => state.user);
   return (
     <div>
       <h1> home Page tittle</h1>
-      {name && <h2> Welcome {name}</h2>}
+      {user.firstName && <h2> Welcome {user.firstName}</h2>}
     </div>
   );
 }
