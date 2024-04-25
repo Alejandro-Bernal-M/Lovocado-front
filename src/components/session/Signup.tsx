25731ccc3dcb2959dfb9aa789fbf48523fe948ab
Alejandro Bernal Marin <alejandrober96@gmail.com>
2024-04-24 19:07:06 -0500

    Add password validation on signup

--- src/components/session/Signup.tsx
+++ src/components/session/Signup.tsx
@@ -1,9 +1,43 @@
 import apiEndPoints from "@/utils/routes";
 
 export default function Signup() {
+  function validatePasswordSecurity() {
+    const password = document.querySelector('input[name="password"]') as HTMLInputElement
+    if(password.value.length < 8){
+      password.style.borderColor = 'red'
+    }
+    else{
+      password.style.borderColor = 'green'
+    }
+  }
+
+  function validatePasswordMatch() {
+    const password = document.querySelector('input[name="password"]') as HTMLInputElement
+    const passwordConfirmation = document.querySelector('input[name="passwordConfirmation"]') as HTMLInputElement
+    if(password.value !== passwordConfirmation.value){
+      passwordConfirmation.style.borderColor = 'red'
+    }
+    else{
+      passwordConfirmation.style.borderColor = 'green'
+    }
+  }
+  
+  function validatePassword(password: string, passwordConfirmation: string) {
+    if(password !== passwordConfirmation){
+      return false
+    }
+    return true
+  }
+
   function handleSignup(e: any) {
     e.preventDefault();
     const data = new FormData(e.target);
+    const password = data.get('password')
+    const passwordConfirmation = data.get('passwordConfirmation')
+    if(!validatePassword(password as string, passwordConfirmation as string)){
+      console.log('passwords do not match')
+      return
+    }
     try {
       fetch(apiEndPoints.signup, {
         method: "POST",
@@ -36,8 +70,8 @@ export default function Signup() {
         <input name="email" type="email" placeholder="email" />
         <input name="username" type="text" placeholder="username" />
         <input name="contactNumber" type="text" placeholder="contact number" />
-        <input name="password" type="password" placeholder="password" />
-        <input name="passwordConfirmation" type="password" placeholder="password confirmation" />
+        <input name="password" type="password" placeholder="password" onKeyUp={validatePasswordSecurity}/>
+        <input name="passwordConfirmation" type="password" placeholder="password confirmation" onKeyUp={validatePasswordMatch}/>
         <input name="profileImage" type="file" />
 
         <button >Signup</button>
