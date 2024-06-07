6d6ec23f18e78c44e79f33557cf6f7bd0b31f0e8
alejandro bernal marin <alejandrober96@gmail.com>
2024-06-07 10:12:34 -0500

    updated routes

--- src/utils/routes.ts
+++ src/utils/routes.ts
@@ -1,17 +1,21 @@
 
 const apiEndPoints = {
-  signup: process.env.NEXT_PUBLIC_API+'/signup',
-  signin: process.env.NEXT_PUBLIC_API+'/signin',
-  getAllProducts: process.env.NEXT_PUBLIC_API+'/products',
-  getProduct: process.env.NEXT_PUBLIC_API+'/product',
-  createProduct: process.env.NEXT_PUBLIC_API+'/products',
-  updateProduct: process.env.NEXT_PUBLIC_API+'/product',
-  deleteProduct: process.env.NEXT_PUBLIC_API+'/product',
-  addReviewToProduct: process.env.NEXT_PUBLIC_API+'/product/review',
-  getCategories: process.env.NEXT_PUBLIC_API+'/categories',
-  createCategory: process.env.NEXT_PUBLIC_API+'/categories',
-  updateCategory: (id: string) => process.env.NEXT_PUBLIC_API+`/categories/${id}`,
-  deleteCategory: (id: string) => process.env.NEXT_PUBLIC_API+`/categories/${id}`,
+  signup: process.env.NEXT_PUBLIC_API+'/signup', // method: POST
+  signin: process.env.NEXT_PUBLIC_API+'/signin', // method: POST
+  getAllProducts: process.env.NEXT_PUBLIC_API+'/products', // method: GET
+  getProduct: process.env.NEXT_PUBLIC_API+'/product', // method: GET
+  createProduct: process.env.NEXT_PUBLIC_API+'/products', // method: POST
+  updateProduct: process.env.NEXT_PUBLIC_API+'/product', // method: PUT
+  deleteProduct: process.env.NEXT_PUBLIC_API+'/product', // method: DELETE
+  addReviewToProduct: process.env.NEXT_PUBLIC_API+'/product/review', // method: POST
+  getCategories: process.env.NEXT_PUBLIC_API+'/categories', // method: GET
+  createCategory: process.env.NEXT_PUBLIC_API+'/categories', // method: POST
+  updateCategory: (id: string) => process.env.NEXT_PUBLIC_API+`/categories/${id}`, // method: PUT
+  deleteCategory: (id: string) => process.env.NEXT_PUBLIC_API+`/categories/${id}`, // method: DELETE
+  addProductToCart: process.env.NEXT_PUBLIC_API+'/cart/add', // method: POST
+  removeProductFromCart: (id: string) => process.env.NEXT_PUBLIC_API+`/cart/remove/${id}`, // method: DELETE
+  clearCart: process.env.NEXT_PUBLIC_API+'/cart/clear', // method: DELETE
+  checkProductsForCheckout: process.env.NEXT_PUBLIC_API+'/cart/checkout', // method: GET
 }
 
 export default apiEndPoints;
