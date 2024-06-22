d048a314924e6d754a673a47ffdb41f4fff4c55f
alejandro bernal marin <alejandrober96@gmail.com>
2024-06-22 08:12:02 -0500

    chore: Update category image size in Admin Categories page

--- src/app/admin/categories/page.tsx
+++ src/app/admin/categories/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 import { useState } from 'react';
 import { useAppSelector } from '@/lib/hooks';
-import CreateCategoryPopup from '@/components/createCategoryPopup/createCategoryPopup';
+import CreateCategoryPopup from '@/components/createCategoryPopup/CreateCategoryPopup';
 
 export default function AdminProducts() {
 
@@ -12,14 +12,14 @@ export default function AdminProducts() {
   return (
     <div>
       <h1>Admin Categories</h1>
-      <button onClick={() => setDisplayCategoryPopup(true)}>Create Category</button>
+      <button onClick={() => setDisplayCategoryPopup(!displayCategoryPopup)}>Create Category</button>
       {displayCategoryPopup && <CreateCategoryPopup />}
       {loading && <p>Loading...</p>}
       {error && <p>{error}</p>}
       {categories && categories.length > 0 && categories.map((category) => (
         <div key={category._id}>
           <h2>{category.name}</h2>
-          <img src={`${process.env.NEXT_PUBLIC_IMAGES}/${category.categoryImage}` } alt={category.name} />
+          <img src={`${process.env.NEXT_PUBLIC_IMAGES}/${category.categoryImage}` } alt={category.name} height={50} width={50}/>
         </div>
       ))}
     </div>
