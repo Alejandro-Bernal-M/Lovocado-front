314a8c7f1926a0ee0567d5c31858147c623dca77
alejandro bernal marin <alejandrober96@gmail.com>
2024-07-12 19:04:02 -0500

    feat: Update homepage with dynamic sections and content

--- src/components/Homepage.tsx
+++ src/components/Homepage.tsx
@@ -1,11 +1,23 @@
 'use client'
 import { useAppSelector} from "../lib/hooks";
+
 export default function Homepage() {
   const {user} = useAppSelector((state) => state.user);
+  const { homeSections } = useAppSelector((state) => state.homeSections);
   return (
     <div>
-      <h1> home Page tittle</h1>
-      {user.firstName && <h2> Welcome {user.firstName}</h2>}
+      <h1>Lovacado web store</h1>
+      {homeSections.map((section) => {
+        return (
+          <div key={section._id}>
+            <h3>{section.title}</h3>
+            <img src={`${process.env.NEXT_PUBLIC_IMAGES}${section.image}`} alt={section.title} width={100}  />
+            {section.paragraphs && section.paragraphs.map((paragraph, index) => {
+              return <p key={index}>{paragraph}</p>
+            })}
+          </div>
+        )
+      })}
     </div>
   );
 }
