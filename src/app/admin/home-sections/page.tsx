0fa80ce4bf4aaf4bfb127b9cd1128c2a5744d3b5
alejandro bernal marin <alejandrober96@gmail.com>
2024-07-14 16:34:14 -0500

    feat: Add update functionality to home section in admin page

--- src/app/admin/home-sections/page.tsx
+++ src/app/admin/home-sections/page.tsx
@@ -1,11 +1,15 @@
 'use client'
 import { useAppSelector, useAppDispatch } from "@/lib/hooks";
-import CreateHomeSectionPopup from "@/components/homeSection/createHomeSectionPopup";
+import CreateHomeSectionPopup from "@/components/homeSection/CreateHomeSectionPopup";
 import { useState } from "react";
+import { HomeSection } from "@/lib/types";
+import EditHomeSectionPopup from "@/components/homeSection/EditHomeSectionPopup";
 
 export default function homeSections(){
   const { homeSections } = useAppSelector((state) => state.homeSections);
   const [showPopup, setShowPopup] = useState(false);
+  const [showUpdatePopup, setShowUpdatePopup] = useState(false);
+  const [selectedHomeSection, setSelectedHomeSection] = useState<HomeSection | null>(null);
   return (
     <div>
       <h1>Home Sections</h1>
@@ -20,10 +24,15 @@ export default function homeSections(){
               return <p key={index}>{paragraph}</p>
             })}
             <p>Order: {section.order}</p>
+            <button onClick={() => {
+              setSelectedHomeSection(section);
+              setShowUpdatePopup(!showUpdatePopup);
+            }}>Edit</button>
           </div>
         )
       })}
       {homeSections.length === 0 && <p>No home sections found</p>}
+      {showUpdatePopup && selectedHomeSection && <EditHomeSectionPopup homeSection={selectedHomeSection} />}
     </div>
   );
 }
